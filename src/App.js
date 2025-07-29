0faa69ba4913418716b14b347797336bf9d9cb0c
vishalpatilmaske <vishpatilmaske@gmail.com>
2025-07-29 12:14:17 +0530

    commit the toaster code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { Toaster } from "react-hot-toast";
-
+// import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return    <>
+  return (
+    <>
       <AppRoutes />
-      <Toaster
-        position="top-right"
-        autoClose={1500}
-        theme="light"
-      />
+      {/* <Toaster position="top-right" autoClose={1500} theme="light" /> */}
     </>
-
+  );
 };
 export default App;
