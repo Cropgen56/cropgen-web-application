5b7fe03493ad81fdb8798c78341d89d9454c0c91
sleepercell38 <insanevishal07@gmail.com>
2025-07-30 15:59:45 +0530

    operation-calender UI completed and Smart Advisory Side bar integrated

--- src/pages/SmartAdviosory.jsx
+++ src/pages/SmartAdviosory.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import SmartAdvisorySidebar from "../components/smartadvisory/smartadvisorysidebar/SmartAdvisorySidebar";
-import "../style/Smartadvisory.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getFarmFields } from "../redux/slices/farmSlice";
+import { useEffect } from "react";
 
 const SmartAdvisory = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state?.auth?.user);
+  const fields = useSelector((state) => state?.farmfield?.fields);
+
+  const userId = user?.id;
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(getFarmFields(userId));
+    }
+  }, [dispatch, userId]);
   return (
-    <div className="smart-advisory container-fluid m-0 p-0 d-flex">
-      <div>
+    <div className="flex h-screen  overflow-hidden bg-[#5a7c6b] text-white">
+      {/* Sidebar (fixed width) */}
+      <div className="w-[280px] h-full border-r border-gray-700">
         <SmartAdvisorySidebar />
       </div>
-      <div className="w-100"> </div>
+
+
+
     </div>
   );
 };
