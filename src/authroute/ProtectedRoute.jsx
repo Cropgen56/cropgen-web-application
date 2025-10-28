8421d80ab2ac658e37648716d43a1161ef46fbb9
sleepercell38 <insanevishal07@gmail.com>
2025-10-28 23:31:13 +0530

    added loader

--- src/authroute/ProtectedRoute.jsx
+++ src/authroute/ProtectedRoute.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { refreshAccessToken } from "../redux/slices/authSlice";
+import LogoFlipLoader from "../components/comman/loading/LogoFlipLoader";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, token, status, refreshPending } = useSelector(
@@ -28,7 +30,24 @@ const ProtectedRoute = ({ children }) => {
   }, [dispatch, token, status, refreshPending, navigate]);
 
   if (isChecking || status === "loading" || refreshPending) {
-    return <div>Loading...</div>;
+    return <div className="flex justify-center items-center h-screen w-full bg-[#344e41] realtive ">
+
+      <AnimatePresence>
+        <motion.div
+          key="loader"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="absolute inset-0  flex flex-col items-center justify-center z-[1000] rounded-2xl"
+        >
+          <LogoFlipLoader />
+          <p className="text-white text-xl mt-2 font-bold animate-pulse">
+            Loading...
+          </p>
+        </motion.div>
+      </AnimatePresence>
+    </div>;
   }
 
   if (!isAuthenticated || !token) {
