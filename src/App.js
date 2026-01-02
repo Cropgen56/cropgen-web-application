350145717da169fe957e135ffe5fbb2a56a5a16c
sleepercell38 <insanevishal07@gmail.com>
2026-01-02 10:04:29 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import React from "react";
-import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
 import { useSelector } from "react-redux";
 // import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  useEffect(() => {
-    const handleWheel = (e) => {
-      // Block page zoom (Ctrl + scroll or pinch)
-      if (e.ctrlKey) {
-        e.preventDefault();
-      }
-    };
-
-    const handleKeyDown = (e) => {
-      // Block Ctrl + (+/-) and Ctrl + 0
-      if (
-        e.ctrlKey &&
-        (e.key === "+" ||
-          e.key === "-" ||
-          e.key === "=" ||
-          e.key === "_" ||
-          e.key === "0")
-      ) {
-        e.preventDefault();
-      }
-    };
-
-    // Add listeners
-    window.addEventListener("wheel", handleWheel, { passive: false });
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("wheel", handleWheel);
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
   const token = useSelector((state) => state.auth.token);
 
   // console.log(token);
@@ -48,4 +15,5 @@ const App = () => {
     </>
   );
 };
+
 export default App;
